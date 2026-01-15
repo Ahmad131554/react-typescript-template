4971f019b4acbaef5bd9804c2d1f2a9758c6cc9e
Ahmad131554 <ahmadhafeez1118@gmail.com>
2026-01-15 12:33:47 +0500

    confilicting port fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
+import { ThemeToggle } from "@/components/common/ThemeToggle";
 
 export default function HomePage() {
   return <div className="">
     <h1>HomePage</h1>
-    
+    <ThemeToggle/>
   </div>;
 }
