937c79fda616b672c5bfe83ecc9fc433c2afbb67
Ahmad131554 <ahmadhafeez1118@gmail.com>
2026-01-15 17:04:06 +0500

    nice custom scrollbar is added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { ThemeToggle } from "@/components/common/ThemeToggle";
 export default function HomePage() {
   return <div className="">
     <h1>HomePage</h1>
+   
     <ThemeToggle/>
   </div>;
 }
